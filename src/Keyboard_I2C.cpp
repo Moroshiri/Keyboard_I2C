1f3d712d9e44b5013013a369f01798e3c3da9b19
Moroshiri <Klaudiusz0006@gmail.com>
2024-01-26 00:18:57 +0100

    First class implementation

--- src/Keyboard_I2C.cpp
+++ src/Keyboard_I2C.cpp
@@ -1,46 +1,99 @@
 #include "Keyboard_I2C.h"
+#include <Arduino.h>
 
-Keyboard_I2C::Keyboard_I2C(uint8_t address) : _expander(new PCF8574(address)), _keysState(0)
-{ }
+#if defined(__AVR)
+
+Keyboard_I2C::Keyboard_I2C(uint8_t address)
+{
+    this->address = address;
+    wire = &Wire;
+}
 
 bool Keyboard_I2C::begin()
 {
-    _expander->pinMode(P0, OUTPUT, LOW);
-    _expander->pinMode(P1, OUTPUT, LOW);
-    _expander->pinMode(P2, OUTPUT, LOW);
-    _expander->pinMode(P3, OUTPUT, LOW);
-    _expander->pinMode(P4, INPUT);
-    _expander->pinMode(P5, INPUT);
-    _expander->pinMode(P6, INPUT);
-    _expander->pinMode(P7, INPUT);
-
-    return _expander->begin();
+    wire->begin();
+    writedPins = 0xFF;
+    return sendByte(writedPins)==0;
 }
 
-uint16_t Keyboard_I2C::getKeysState() const
+uint8_t Keyboard_I2C::sendByte(uint8_t value)
 {
-    return _keysState;
+  wire->beginTransmission(address);
+  wire->write(value);
+  return wire->endTransmission();
 }
 
-void Keyboard_I2C::scanKeyboard()
+void Keyboard_I2C::setByte(uint8_t value)
 {
-    _keysState = 0;
-    for(uint8_t r = 0; r < KEY_HEIGHT; r++)
+  sendByte(value);
+  writedPins = value;
+}
+
+uint8_t Keyboard_I2C::getByte()
+{
+  #if defined(DEBUG)
+  Serial.println(F("Reading"));
+  #endif
+
+  uint8_t result;
+  wire->requestFrom(address, (uint8_t)1);
+
+  #if defined(DEBUG)
+  Serial.println(F("Requested"));
+  #endif
+
+  if(wire->available())
+    result = wire->read();
+  
+  #if defined(DEBUG)
+  Serial.println(F("Readed"));
+  Serial.print("Result: ");
+  Serial.println(result);
+  #endif
+
+  return result;
+}
+
+void Keyboard_I2C::setPin(uint8_t pin, uint8_t value)
+{
+  uint8_t pinMask = bit(pin);
+  if (value)
+    writedPins |= pinMask;
+  else
+    writedPins &= ~pinMask;
+
+  sendByte(writedPins);
+}
+
+uint8_t Keyboard_I2C::getPin(uint8_t pin)
+{
+  return (getByte() & bit(pin)) >> pin;
+}
+
+uint16_t Keyboard_I2C::scanKeyboard()
+{
+    uint16_t keysState = 0;
+    for(uint8_t r = 0; r < 4; r++)
     {
-        _expander->digitalWrite(r, HIGH);
-        for(uint8_t c = 0; c < KEY_WIDTH; c++)
+        setPin(r + R1, LOW);
+        for(uint8_t c = 0; c < 4; c++)
         {
-            uint8_t value = _expander->digitalRead(c + P4);
+            uint8_t value = getPin(c + C1);
             
+            #if defined(TRYKEYS)
             Serial.print("R: ");
             Serial.print(r);
             Serial.print(" C: ");
             Serial.print(c);
             Serial.print(" V: ");
             Serial.println(value);
+            #endif
 
-            _keysState |= value << (c + r*KEY_HEIGHT);
+            keysState |= !value << (c + r*4);
         }
-        _expander->digitalWrite(r, LOW);
+        setPin(r + R1, HIGH);
     }
+    return keysState;
 }
+
+#endif
--- src/Keyboard_I2C.h
+++ src/Keyboard_I2C.h
@@ -1,11 +1,20 @@
 #ifndef Keyboard_I2C_h
 #define Keyboard_I2C_h
 
-#include <PCF8574.h>
+#include <Wire.h>
 
 #define KEY_WIDTH 4
 #define KEY_HEIGHT 4
 #define KEY_BUTTONS 16
+
+#define R1 4
+#define R2 5
+#define R3 6
+#define R4 7
+#define C1 0
+#define C2 1
+#define C3 2
+#define C4 3
 
 class Keyboard_I2C
 {
@@ -13,12 +22,18 @@ public:
     Keyboard_I2C(uint8_t address);
 
     bool begin();
-    uint16_t getKeysState() const;
-    void scanKeyboard();
+    uint16_t scanKeyboard();
 
 private:
-    PCF8574* _expander;
-    uint16_t _keysState;
+    uint8_t address;
+    uint8_t writedPins;
+    TwoWire* wire;
+
+    uint8_t sendByte(uint8_t value);
+    void setByte(uint8_t value);
+    uint8_t getByte();
+    void setPin(uint8_t pin, uint8_t value);
+    uint8_t getPin(uint8_t pin);
 };
 
 #endif
